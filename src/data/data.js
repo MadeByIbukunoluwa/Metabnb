122b2338772518d7ef30147391839d43845f9a6d
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2022-11-11 01:12:20 +0100

    styling: added default global styling to App.css

--- src/data/data.js
+++ src/data/data.js
@@ -77,7 +77,7 @@ rating:5,
     place:"",
     price:"1MBT per night",
     distance:"2345km",
-    availability:"available for 2 weeks stay";
+    availability:"available for 2 weeks stay",
     rating:5,
 }
    ,
