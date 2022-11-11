112205e3f51ade4b7a9b765840136374c6a1bad3
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2022-11-11 01:00:20 +0100

    modify : modifed data.js file

---
+++ src/data/data.js
@@ -0,0 +1,176 @@
+
+
+const data = [
+    {
+    image:"",
+    name:"Desert King",
+    place:"",
+    price:"1MBT per night",
+    distance:"2345km",
+    availability:"available for 2 weeks stay",
+    rating:5,
+    }
+   ,
+    {
+    image:"",
+    name:"Desert King",
+    place:"",
+    price:"1MBT per night",
+    distance:"2345km",
+    availability:"available for 2 weeks stay",
+rating:5,
+    }
+   ,
+    {
+    image:"",
+    name:"Desert King",
+    place:"",
+    price:"1MBT per night",
+    distance:"2345km",
+    availability:"available for 2 weeks stay",
+rating:5,
+    }
+   ,
+    {
+    image:"",
+    name:"Desert King",
+    place:"",
+    price:"1MBT per night",
+    distance:"2345km",
+    availability:"available for 2 weeks stay",
+    rating:5,
+    }
+   ,
+    {
+    image:"",
+    name:"Desert King",
+    place:"",
+    price:"1MBT per night",
+    distance:"2345km",
+    availability:"available for 2 weeks stay",
+    rating:5,
+    }
+   ,
+    {
+    image:"",
+    name:"Desert King",
+    place:"",
+    price:"1MBT per night",
+    distance:"2345km",
+    availability:"available for 2 weeks stay",
+    rating:5,
+    }
+   ,
+    {
+    image:"",
+    name:"Desert King",
+    place:"",
+    price:"1MBT per night",
+    distance:"2345km",
+    availability:"available for 2 weeks stay",
+    rating:5,
+    }
+   ,
+    {
+    image:"",
+    name:"Desert King",
+    place:"",
+    price:"1MBT per night",
+    distance:"2345km",
+    availability:"available for 2 weeks stay";
+    rating:5,
+}
+   ,
+    {
+    image:"",
+    name:"Desert King",
+    place:"",
+    price:"1MBT per night",
+    distance:"2345km",
+    availability:"available for 2 weeks stay",
+    rating:5,
+    }
+   ,
+    {
+    image:"",
+    name:"Desert King",
+    place:"",
+    price:"1MBT per night",
+    distance:"2345km",
+    availability:"available for 2 weeks stay",
+    rating:5,
+    }
+   ,
+    {
+    image:"",
+    name:"Desert King",
+    place:"",
+    price:"1MBT per night",
+    distance:"2345km",
+    availability:"available for 2 weeks stay",
+    rating:5,
+    }
+   ,
+    {
+    image:"",
+    name:"Desert King",
+    place:"",
+    price:"1MBT per night",
+    distance:"2345km",
+    availability:"available for 2 weeks stay",
+    rating:5,
+
+    }
+   ,
+    {
+    image:"",
+    name:"Desert King",
+    place:"",
+    price:"1MBT per night",
+    distance:"2345km",
+    availability:"available for 2 weeks stay",
+    rating:5,
+    }
+   ,
+    {
+    image:"",
+    name:"Desert King",
+    place:"",
+    price:"1MBT per night",
+    distance:"2345km",
+    availability:"available for 2 weeks stay",
+    rating:5,
+    }
+   ,
+    {
+    image:"",
+    name:"Desert King",
+    place:"",
+    price:"1MBT per night",
+    distance:"2345km",
+    availability:"available for 2 weeks stay",
+    rating:5,
+    }
+   ,
+    {
+    image:"",
+    name:"Desert King",
+    place:"",
+    price:"1MBT per night",
+    distance:"2345km",
+    availability:"available for 2 weeks stay",
+    rating:5,
+    }
+   ,
+    {
+    image:"",
+    name:"Desert King",
+    place:"",
+    price:"1MBT per night",
+    distance:"2345km",
+    availability:"available for 2 weeks stay",
+    rating:5,
+    }
+   ,
+
+]
