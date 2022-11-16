83b715329dd51a173c1c807ceea8a4bb3515ba1b
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2022-11-16 08:55:42 +0100

    fix : corrected file name path in initial deploy 1

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from '../src/pages/App/App'
-import PlaceToStay from './pages/PlaceToStay/PlaceToStay';
+import PlaceToStay from './pages/PlaceToStay/placeToStay';
 import ErrorPage from "./error-page";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
