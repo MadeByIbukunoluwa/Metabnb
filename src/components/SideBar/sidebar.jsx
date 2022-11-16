fde08511c79f0957a91c96aba389075866ea2850
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2022-11-16 08:09:50 +0100

    feat:added close button

--- src/components/SideBar/sidebar.jsx
+++ src/components/SideBar/sidebar.jsx
@@ -6,28 +6,35 @@ import '../../pages/App/App.css'
 import Button from "../Button/button";
 import { AppContext } from "../../pages/App/App";
 import classes from './sidebar.module.css'
+import {TfiClose} from 'react-icons/tfi'
 function Sidebar() {
     const { isDrawerOpen, setIsDrawerOpen,openModal } = useContext(AppContext);
   return (
     <Drawer
-    sx={{
-         width:"50vw"
-    }}
+      sx={{
+        width: "50vw",
+        position:'relative',
+      }}
       anchor="right"
       open={isDrawerOpen}
       onClose={() => setIsDrawerOpen(false)}
     >
+      <TfiClose onClick={() => setIsDrawerOpen(false)}
+      style = {{
+        position:'absolute',
+        right:'30px',
+        top:'30px'
+      }}
+       />
       <nav className={classes.sidebar_nav}>
-          {navLinks.map(({ name, link, id }) => {
-            return (
-              <Link key={id} to={link}>
-                {name}
-              </Link>
-            );
-          })}
-        <Button onClick={openModal}>
-         Connect Wallet 
-        </Button>
+        {navLinks.map(({ name, link, id }) => {
+          return (
+            <Link key={id} to={link}>
+              {name}
+            </Link>
+          );
+        })}
+        <Button onClick={openModal}>Connect Wallet</Button>
       </nav>
     </Drawer>
   );
